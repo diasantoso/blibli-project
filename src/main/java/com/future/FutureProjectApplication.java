927e848e8d180634a8b50ceac588f73ed27fb3ce
diasantoso <mikhaelmds@gmail.com>
2017-03-11 13:05:50 +0700

    making project

---
+++ src/main/java/com/future/FutureProjectApplication.java
@@ -0,0 +1,12 @@
+package com.future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureProjectApplication.class, args);
+	}
+}
