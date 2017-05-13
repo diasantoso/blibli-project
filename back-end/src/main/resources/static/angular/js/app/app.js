55623e55b2871df42ab0ff3df76275bc66b688ae
diasantoso <mikhaelmds@gmail.com>
2017-05-13 15:45:58 +0700

    Made Controller, Service and View for Booking

--- back-end/src/main/resources/static/angular/js/app/app.js
+++ back-end/src/main/resources/static/angular/js/app/app.js
@@ -90,6 +90,6 @@ app.config(['$stateProvider', '$urlRouterProvider',
                             return deferred.promise;
                         }
                     }
-                })
+                });
 
        $urlRouterProvider.otherwise('/');}]);
